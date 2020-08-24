7b2caa3420d29573fa1f8aa4a2deef588422642e
zepang <ummlq4@gmail.com>
2020-08-24 21:00:18 +0800

    'fix'

--- code/js/poly-search.js
+++ code/js/poly-search.js
@@ -1,31 +1,66 @@
+const API_KEY = 'AIzaSyDAVRwVpYVdrLrhVvnJbmsM6LB9wzlf-O4'
+
 class PolySearch extends HTMLElement {
+  static get observedAttributes () {
+    return ['searchterm']
+  }
   constructor () {
     super()
     this.API_KEY = 'AIzaSyDAVRwVpYVdrLrhVvnJbmsM6LB9wzlf-O4'
   }
 
-  connectedCallback () {
+  set apiKey (value) {
+    this._apiKey = value
+    this.doSearch() 
+  }
+
+  set searchTerm (value) {
+    this._searchTerm = value
     this.doSearch()
   }
 
+  connectedCallback () {
+    if (this.hasAttribute('thumbheight')) {
+      this._thumbheight = this.getAttribute('thumbheight')
+      this._thumbwidth = this.getAttribute('thumbheight') * 1.3333
+    } else {
+      this._thumbheight = 150
+      this._thumbwidth = 200
+    }
+
+    if (this.hasAttribute('backgroundcolor')) {
+      this.style.backgroundColor = this.getAttribute('backgroundcolor')
+    }
+
+    this.doSearch()
+  }
+
+  attributeChangedCallback (name, oldValue, newValue) {
+    console.log(name)
+    if (name === 'searchterm' && oldValue !== newValue) {
+      this.doSearch()
+    }
+  }
+
   doSearch () {
-    const API_KEY = this.API_KEY
-    const url = `https://poly.googleapis.com/v1/assets?keywords=parrot&format=OBJ&key=${API_KEY}`
+    if (this.getAttribute('apiKey') && this.getAttribute('searchTerm')) {
+      const url = `${thsi.getAttribute('baseuri')}?keywords=${this.getAttribute('searchTerm')}&format=OBJ&key=${this.getAttribute('apiKey')}`
 
-    const request = new XMLHttpRequest()
-    request.open('GET', url, true)
-    request.addEventListener('load', event => {
-      const assets = JSON.parse(event.target.response).assets 
-      this.renderResult(assets)
-    })
+      const request = new XMLHttpRequest()
+      request.open('GET', url, true)
+      request.addEventListener('load', event => {
+        const assets = JSON.parse(event.target.response).assets 
+        this.renderResult(assets)
+      })
 
-    request.send()
+      request.send()
+    }
   }
 
   renderResult (assets) {
     let htmlStr = ''
     for (let i= 0; i < assets.length; i++) {
-      htmlStr += `<img src="${assets[i].thumbnail.url}" width="200" height="150"/>`
+      htmlStr += `<img src="${assets[i].thumbnail.url}" width="${this._thumbwidth}" height="${this._thumbheight}"/>`
     }
 
     this.innerHTML = htmlStr
@@ -35,3 +70,6 @@ class PolySearch extends HTMLElement {
 if (!customElements.get('poly-search')) {
   customElements.define('poly-search', PolySearch)
 }
+
+document.querySelector('poly-search').apiKey = API_KEY
+document.querySelector('poly-search').searchTerm = 'parrot'
