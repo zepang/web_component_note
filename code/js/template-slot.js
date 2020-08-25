3afe11b6ff7bb4c406ba2782418127c8ed4fb952
zepang <ummlq4@gmail.com>
2020-08-25 20:56:52 +0800

    '使用get和set配合attributeChangedCallback'

--- code/js/template-slot.js
+++ code/js/template-slot.js
@@ -1,4 +1,24 @@
 class SliderBar extends HTMLElement {
+  static get observedAttributes () {
+    return ['value', 'backgroudcolor']
+  }
+
+  set value (value) {
+    this.setAttribute('value', value)
+  }
+
+  get value () {
+    return this.getAttribute('value')
+  }
+
+  set backgroundcolor (value) {
+    this.setAttribute('backgroundcolor', value)
+  }
+
+  get backgroundcolor () {
+    return this.getAttribute('backgroundcolor')
+  }
+
   constructor () {
     super()
     this.template = document.getElementById('slider-bar').content
@@ -12,8 +32,33 @@ class SliderBar extends HTMLElement {
   }
 
   connectedCallback () {
-    this.shadowRoot.querySelector('.bg-overlay').style.backgroundColor = this.getAttribute('background-color')
-    this.shadowRoot.querySelector('.thumb').style.marginLeft = `${this.getAttribute('value')}px`
+    this.setColor(this.backgroundcolor)
+    this.refreshSlider(this.value)
+  }
+
+  attributeChangedCallback (name, oldValue, newValue) {
+    switch(name) {
+      case 'value': 
+        this.refreshSlider(newValue)
+        break
+      case 'backgroundcolor':
+        debugger
+        tgus.setColor(newValue)
+        break
+    }
+  }
+
+  setColor (color) {
+    console.log(color)
+    if (this.shadowRoot.querySelector('.bg-overlay')) {
+      this.shadowRoot.querySelector('.bg-overlay').style.backgroundColor = `linear-gradient(to right, ${color} 0%, ${color}00 100%)`
+    }
+  }
+
+  refreshSlider (value) {
+    if (this.shadowRoot.querySelector('.thumb')) {
+      this.shadowRoot.querySelector('.thumb').style.left = `${value/100 * (this.offsetWidth - this.shadowRoot.querySelector('.thumb').offsetWidth / 2)}px `
+    }
   }
 }
 
